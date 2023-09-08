LineageOS/android_packages_apps_Aperture

Aperture: Use the view model for views' rotation
Change-Id: I97f5895e182b7d63dc44b2b7cd24ede933d8bf0d

app/src/main/java/org/lineageos/aperture/CameraActivity.kt
@@ -588,6 +588,10 @@ open class CameraActivity : AppCompatActivity() {
         cameraMode = overrideInitialCameraMode() ?: sharedPreferences.lastCameraMode
         initialCameraFacing = sharedPreferences.lastCameraFacing
 
+        // Pass the view model to the views
+        capturePreviewLayout.cameraViewModel = cameraViewModel
+        countDownView.cameraViewModel = cameraViewModel
+
         // Restore settings from shared preferences
         gridMode = sharedPreferences.lastGridMode
         timerMode = sharedPreferences.timerMode
@@ -2072,12 +2076,6 @@ open class CameraActivity : AppCompatActivity() {
         // Rotate sliders
         exposureLevel.screenRotation = screenRotation
         zoomLevel.screenRotation = screenRotation
-
-        // Rotate countdown
-        countDownView.screenRotation = screenRotation
-
-        // Rotate capture preview buttons
-        capturePreviewLayout.screenRotation = screenRotation
 
         // Rotate secondary top bar buttons
         ConstraintLayout::class.safeCast(

ProjectBlaze/packages_apps_Aperture

app/src/main/java/org/lineageos/aperture/CameraActivity.kt
@@ -528,6 +528,10 @@ open class CameraActivity : AppCompatActivity() {
         cameraMode = overrideInitialCameraMode() ?: sharedPreferences.lastCameraMode
         initialCameraFacing = sharedPreferences.lastCameraFacing
 
+        // Pass the view model to the views
+        capturePreviewLayout.cameraViewModel = model
+        countDownView.cameraViewModel = model
+
         // Restore settings from shared preferences
         gridMode = sharedPreferences.lastGridMode
         timerMode = sharedPreferences.timerMode
@@ -2010,12 +2014,6 @@ open class CameraActivity : AppCompatActivity() {
         // Rotate sliders
         exposureLevel.screenRotation = screenRotation
         zoomLevel.screenRotation = screenRotation
-
-        // Rotate countdown
-        countDownView.screenRotation = screenRotation
-
-        // Rotate capture preview buttons
-        capturePreviewLayout.screenRotation = screenRotation
 
         // Rotate secondary top bar buttons
         ConstraintLayout::class.safeCast(